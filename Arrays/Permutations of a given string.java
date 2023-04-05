1271e00ed99086a40f2c73b99d9b236b4f4f7543
Sudhanshu Kumar <84763877+Sudhanshu2829@users.noreply.github.com>
2023-04-06 00:01:38 +0530

    Update Permutations of a given string.java

--- Arrays/Permutations of a given string.java
+++ Arrays/Permutations of a given string.java
@@ -1,1 +1,38 @@
-
+class Solution {
+    public void swap(char arr[],int i,int j)
+    {
+        char ch=arr[i];
+        arr[i]=arr[j];
+        arr[j]=ch;
+    }
+    public void solve(int ind,char[] arr,HashSet<String>st)
+    {
+        if(ind>=arr.length)
+        {
+            String s=String.valueOf(arr);
+            st.add(s);
+            return;
+        }
+        for(int i=ind;i<arr.length;i++)
+        {
+        swap(arr,i,ind);
+            solve(ind+1,arr,st);
+            swap(arr,i,ind);
+        }
+    }
+    public List<String> find_permutation(String S) {
+        // Code here
+        int n=S.length();
+       
+       HashSet<String>st=new HashSet<>();
+        List<String> l=new ArrayList<>();
+        
+         char[]arr=S.toCharArray();
+        solve(0,arr,st);
+        for(String s:st)
+        l.add(s);
+      
+      Collections.sort(l);
+        return l;
+    }
+}
