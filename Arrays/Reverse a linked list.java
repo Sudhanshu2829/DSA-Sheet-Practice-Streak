47394ff23764e5a2c95df2f0ab3e69a9091396c7
Sudhanshu Kumar <84763877+Sudhanshu2829@users.noreply.github.com>
2023-04-16 23:48:54 +0530

    Add files via upload

---
+++ Arrays/Reverse a linked list.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        Node prev=null;
+        Node curr=head;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    
+    return head;}
+}
