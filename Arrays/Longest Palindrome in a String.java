9d8fb31986fdfef392a5a9ba1f0df83829948eb9
Sudhanshu Kumar <84763877+Sudhanshu2829@users.noreply.github.com>
2023-05-29 00:02:54 +0530

    Update Longest Palindrome in a String.java

--- Arrays/Longest Palindrome in a String.java
+++ Arrays/Longest Palindrome in a String.java
@@ -1,1 +1,50 @@
-
+class Solution{
+    static String longestPalin(String S){
+        // code here
+        
+        String x = "";
+		x += S.charAt(0);
+		int l = 0;
+		int n = S.length();
+		
+	    for(int i=0;i<S.length();i++) {
+			int i1 = i;
+			int i2 = i+1;
+			while(i1>=0 && i2<n) {
+				if(S.charAt(i1) == (S.charAt(i2))) {
+					int length = i2-i1+1;
+					if(length>l) {
+						l = length;
+						x = S.substring(i1,i1+length);
+					}
+					
+				}else{
+				    break;
+				}
+				i1--;
+				i2++;
+			}
+		}
+	
+	    for(int i=0;i<S.length();i++) {
+			int i1 = i-1;
+			int i2 = i+1;
+			while(i1>=0 && i2<n) {
+				if(S.charAt(i1) == (S.charAt(i2))) {
+					int length = i2-i1+1;
+					if(length>l) {
+						l = length;
+						x = S.substring(i1,i1+length);
+					}
+					
+				}else{
+				    break;
+				}
+				i1--;
+				i2++;
+			}
+		}
+		return x;
+        
+    }
+}
